a426880f34df3913981fc02a52741eb871cff4fb
Serhii Popil <sergii.popil@gmail.com>
2023-09-14 22:02:39 +0300

    create class User and enum GenderType

---
+++ Classes/User.cs
@@ -0,0 +1,42 @@
+﻿using HomeTask4_Classes.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask4_Classes.Classes
+{
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private int _age { get; set; }
+        public bool IsBaby
+        {
+            get
+            {
+                return _age < 10;
+            }
+        }
+        public GenderType Gender { get; set; }
+        public User()
+        {
+        }
+        public User(int age)
+        {
+            _age = age;
+        }
+        public override string ToString()
+        {
+            if (IsBaby)
+            {
+                return $"Hi, my name is {FirstName} and last name {LastName}. I am a baby. I am {Gender}.";
+            }
+            else
+            {
+                return $"Hi, my name is {FirstName} and last name {LastName}. I am {_age} years old. I am {Gender}.";
+            }
+        }
+    }
+}
---
+++ Enums/GenderType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask4_Classes.Enums
+{
+    enum GenderType
+    {
+        None,
+        Male,
+        Female
+    }
+}
