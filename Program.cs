07d6891f088020883c8676a26f52e1fbe27a9518
Serhii Popil <sergii.popil@gmail.com>
2023-09-14 22:21:19 +0300

    Methods to get user Age from birth year

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using HomeTask4_Classes.Classes;
+int age = 0;
+CalcAge();
+User user1 = new User(age);
+
+
+
+#region Methods to get birth date and calculate user age
+void CalcAge()
+{
+    try
+    {
+        GetAge();
+    }
+    catch
+    {
+        Console.Write("\n!!! Please enter only digits !!!\n");
+        CalcAge();
+    }
+}
+void GetAge()
+{
+    while (age <= 0 || age > 100)
+    {
+        Console.Write("Please enter your birth year(only by numbers in format XXXX):");
+        int birthYear = Convert.ToInt32(Console.ReadLine());
+        age = DateTime.Now.Year - birthYear;
+    }
+}
+#endregion
