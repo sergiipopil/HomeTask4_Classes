e5ed0b801dcbb229c8dd272a9f447de536a5e7ce
Serhii Popil <sergii.popil@gmail.com>
2023-09-14 23:09:01 +0300

    Method for get user Gender type

--- Program.cs
+++ Program.cs
@@ -1,10 +1,38 @@
 ﻿using HomeTask4_Classes.Classes;
+using HomeTask4_Classes.Enums;
+using System;
+
+//init age
 int age = 0;
+
+//init genderType
+
+
+//call method to calc age from user birth year
 CalcAge();
+
 User user1 = new User(age);
+user1.Gender = GetUserGenderType();
+Console.WriteLine(user1.Gender);
 
-
-
+#region Method to get Gender enum type
+GenderType GetUserGenderType()
+{
+    GenderType gender = GenderType.None;
+    while (true)
+    {
+        Console.Write("\nPlease enter your gender - Male or Female:");
+        string genderType = Console.ReadLine();
+        Enum.TryParse(genderType, true, out gender);
+        bool isMatch = Enum.IsDefined(typeof(GenderType), gender);
+        if (isMatch && gender != GenderType.None)
+        {
+            break;
+        }
+    }
+    return gender;
+}
+#endregion
 #region Methods to get birth date and calculate user age
 void CalcAge()
 {
